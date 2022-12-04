f28f29ea4bcf271c2ce25902bc2481eb97e4e0da
Steve Cross <flip@foxhollow.cc>
2022-12-04 16:33:44 -0500

    Started building callback-based media scanner
    currently it supports filtering by include_paths, exclude_paths, and extensions. Next will be support to handle the situation when a directory is renamed during the scan

--- commands/organize_raw.py
+++ commands/organize_raw.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import click
 import json
 import os
@@ -38,155 +39,151 @@ def command() -> None:
     # 3 = camera folder
     # 4 or higher = invalid
 
-    for (file, depth) in shared.scantree(Config.directories.raw_footage_root):
-        if file.is_file():
-            path = pathlib.Path(file.path)
+    def handle_media_file(entry: os.DirEntry, depth: int, dir_renamed: Callable) -> None:
+        path = pathlib.Path(entry.path)
 
-            extension = path.suffix[1:]
-            file_basename = path.name[:-len(extension) - 1]
-
-            # skip any extensions that are not defined by a camera profile
-            if extension not in Config.known_extensions:
-                continue
-
-            # skip any paths that are defined in our ignore_paths directive
-            if any(file.path.startswith(x) for x in Config.directories.ignore_paths):
-                continue
-
-            # if a list of include paths is configued, we require the path to be in the list
-            # else we skip the current path
-            if len(Config.directories.include_paths) > 0:
-                if not any(file.path.startswith(x) for x in Config.directories.include_paths):
-                    continue
-
-            file_path_friendly = f"{depth}_raw:" + file.path.removeprefix(Config.directories.raw_footage_root)
-            print('')
-            print(f'>> [magenta]File[/magenta]        : [dark_blue]{file_path_friendly}[/dark_blue]')
-
-            # now we build some logic to to determine if the video is organized properly
-            # into a known camera folder. camera folders currently live at a depth of 3
-            in_cam_directory = depth == 3
-            dir_is_known_cam = False
-            cam_dir_name = ''
-
-            # if we are deemed to be in a cam folder, lets check to see if it is a known camera
-            if in_cam_directory:
-                cam_dir_name = path.parent.name
-                dir_is_known_cam = cam_dir_name in Config.cam_name_mappings.values()
-
-            print(f'   [magenta]Folder Stats[/magenta]: in cam folder: {in_cam_directory} / '
-                  f'cam dir name: \'{cam_dir_name}\' / known_cam: {dir_is_known_cam}')
-
-            if dir_is_known_cam and not rescan_known_cam_dirs:
-                print_ident_result('Known camera directory, nothing to do', 'green')
-                continue
-
-            # Read metadata using `mediainfo` tool
-            proc = subprocess.run(['mediainfo', '--Output=JSON', file.path], stdout=subprocess.PIPE)
-
-            minfo = json.loads(proc.stdout)
-            scorecard = csig.new_scorecard()
-
-            tracks = minfo['media']['track']
-
-            for cam_id in Config.cam_profiles:
-                cam = Config.cam_profiles[cam_id]
-
-                # TODO: use provided (de)commission dates as binary qualifiers
-
-                scorecard.process_section_hints(cam_id, 'General', tracks, cam.hints)
-                scorecard.process_section_hints(cam_id, 'Video', tracks, cam.hints)
-                scorecard.process_section_hints(cam_id, 'Audio', tracks, cam.hints)
-                scorecard.process_file_extension(cam_id, extension, cam.hints)
-                scorecard.process_file_pattern(cam_id, file_basename, cam.hints)
-
-            top_scores = scorecard.get_top_scores(2)
-            confidence = scorecard.calc_confidence()
-            confidence_pass = confidence > static.required_confidence
-
-            c1 = top_scores[0]
-            c1_score = round(100 * (c1[1] / static.max_score), 0)
-            m1 = f'{c1[0]} / C: {round(confidence, 1)}% / S: {c1_score}%'
-            m2 = ''
-
-            if len(top_scores) > 1:
-                c2 = top_scores[1]
-                c2_score = round(100 * (c2[1] / static.max_score), 0)
-                m2 = f'[dim][2nd: {c2[0]} / S: {c2_score}%][/]'
-
-            print(f'   [magenta]Cam Profile[/magenta] : {m1} {m2}   Pass: {confidence_pass}')
-
-            identified_cam_name = Config.cam_name_mappings[top_scores[0][0]]
+        extension = path.suffix[1:]
+        file_basename = path.name[:-len(extension) - 1]
 
 
-            # Analyze directory structure and take action as needed to organize raw media
-            if depth not in [2, 3]:
-                stats['located_at_invalid_level'] += 1
-                print_ident_result(f'File exists at unknown depth \'{depth}\'', 'red')
-                continue
+        file_path_friendly = f"{depth}_raw:" + entry.path.removeprefix(Config.directories.raw_footage_root)
+        print('')
+        print(f'>> [magenta]File[/magenta]        : [dark_blue]{file_path_friendly}[/dark_blue]')
 
-            # The file was located in a camera directory, which may or may not be known
-            if in_cam_directory:
+        # now we build some logic to to determine if the video is organized properly
+        # into a known camera folder. camera folders currently live at a depth of 3
+        in_cam_directory = depth == 3
+        dir_is_known_cam = False
+        cam_dir_name = ''
 
-                # the cam directory is a known camera profile
-                if dir_is_known_cam:
+        # if we are deemed to be in a cam folder, lets check to see if it is a known camera
+        if in_cam_directory:
+            cam_dir_name = path.parent.name
+            dir_is_known_cam = cam_dir_name in Config.cam_name_mappings.values()
 
-                    # The scanner confidently identified the camera of the file
-                    if confidence_pass:
+        print(f'   [magenta]Folder Stats[/magenta]: in cam folder: {in_cam_directory} / '
+              f'cam dir name: \'{cam_dir_name}\' / known_cam: {dir_is_known_cam}')
 
-                        # The identified camera matches the name of the dir, all done here!
-                        if identified_cam_name == cam_dir_name:
-                            stats['in_proper_directory'] += 1
-                            print_ident_result('File in proper directory, nothing to do', 'green')
+        if dir_is_known_cam and not rescan_known_cam_dirs:
+            print_ident_result('Known camera directory, nothing to do', 'green')
+            return
 
-                        # TODO: Do we want to auto rename if the dir is named wrong but we confidently know the cam profile?
-                        # The identified camera does NOT match the name of the dir, that means we have a mis-named directory
-                        # that needs to be corrected manually by the user
-                        else:
-                            stats['in_cam_dir_not_correct_cam'] += 1
-                            print_ident_result('File in known cam dir, but cam not same as identified', 'red')
+        # TODO: Move this functionality to a separate function
+        # Read metadata using `mediainfo` tool
+        proc = subprocess.run(['mediainfo', '--Output=JSON', entry.path], stdout=subprocess.PIPE)
 
-                    # The file is in a known camera dir but our scanner is not confident enough to confirm it. This means
-                    # the file was organized manually by the user and the resuls must be trusted
+        minfo = json.loads(proc.stdout)
+        scorecard = csig.new_scorecard()
+
+        tracks = minfo['media']['track']
+
+        for cam_id in Config.cam_profiles:
+            cam = Config.cam_profiles[cam_id]
+
+            # TODO: use provided (de)commission dates as binary qualifiers
+            
+            scorecard.process_section_hints(cam_id, 'General', tracks, cam.hints)
+            scorecard.process_section_hints(cam_id, 'Video', tracks, cam.hints)
+            scorecard.process_section_hints(cam_id, 'Audio', tracks, cam.hints)
+            scorecard.process_file_extension(cam_id, extension, cam.hints)
+            scorecard.process_file_pattern(cam_id, file_basename, cam.hints)
+
+        top_scores = scorecard.get_top_scores(2)
+        confidence = scorecard.calc_confidence()
+        confidence_pass = confidence > static.required_confidence
+
+        c1 = top_scores[0]
+        c1_score = round(100 * (c1[1] / static.max_score), 0)
+        m1 = f'{c1[0]} / C: {round(confidence, 1)}% / S: {c1_score}%'
+        m2 = ''
+
+        if len(top_scores) > 1:
+            c2 = top_scores[1]
+            c2_score = round(100 * (c2[1] / static.max_score), 0)
+            m2 = f'[dim][2nd: {c2[0]} / S: {c2_score}%][/]'
+
+        print(f'   [magenta]Cam Profile[/magenta] : {m1} {m2}   Pass: {confidence_pass}')
+
+        identified_cam_name = Config.cam_name_mappings[top_scores[0][0]]
+
+
+        # Analyze directory structure and take action as needed to organize raw media
+        if depth not in [2, 3]:
+            stats['located_at_invalid_level'] += 1
+            print_ident_result(f'File exists at unknown depth \'{depth}\'', 'red')
+            return
+
+        # The file was located in a camera directory, which may or may not be known
+        if in_cam_directory:
+
+            # the cam directory is a known camera profile
+            if dir_is_known_cam:
+
+                # The scanner confidently identified the camera of the file
+                if confidence_pass:
+
+                    # The identified camera matches the name of the dir, all done here!
+                    if identified_cam_name == cam_dir_name:
+                        stats['in_proper_directory'] += 1
+                        print_ident_result('File in proper directory, nothing to do', 'green')
+
+                    # TODO: Do we want to auto rename if the dir is named wrong but we confidently know the cam profile?
+                    # The identified camera does NOT match the name of the dir, that means we have a mis-named directory
+                    # that needs to be corrected manually by the user
                     else:
-                        stats['in_proper_directory_trusted'] += 1
-                        print_ident_result('File in known cam directory but no confidence match, assume correct and nothing to do.', 'blue')
+                        stats['in_cam_dir_not_correct_cam'] += 1
+                        print_ident_result('File in known cam dir, but cam not same as identified', 'red')
 
-                # the cam directory is NOT a known camera profile
+                # The file is in a known camera dir but our scanner is not confident enough to confirm it. This means
+                # the file was organized manually by the user and the resuls must be trusted
                 else:
+                    stats['in_proper_directory_trusted'] += 1
+                    print_ident_result('File in known cam directory but no confidence match, assume correct and nothing to do.', 'blue')
 
-                    # The camera scanner confidence is high enough to rename the directory automatically
-                    if confidence_pass:
-                        stats['in_unknown_cam_dir_movable'] += 1
-                        # print_ident_result('File in unknown cam directory.. dir can be renamed automatically', 'yellow')
-                        new_dir_name = identified_cam_name
-                        new_full_path = os.path.join(path.parent.parent.resolve(), new_dir_name)
-
-                        # rename the parent (directory) to the new name
-                        # TODO: Enable renaming once the directory scanning logic has been integrated directly into the identify function
-                        # path.parent.rename(new_full_path)
-                        print_ident_result(f'File in unknown cam directory.. dir renamed to \'{new_dir_name}\'', 'yellow')
-
-                    # the camera scanner confidence is NOT high enough to take any automatic action. manual user intervention required
-                    else:
-                        stats['in_unknown_cam_dir_not_movable'] += 1
-                        print_ident_result('File in unknown cam directory.. dir cannot be renamed automatically', 'red')
-
-            # Discovered file not located in an appropriately named camera directory.. action needs to be taken
+            # the cam directory is NOT a known camera profile
             else:
 
-                # We are confident of our camera identification, move the file to an appropriately named folder
+                # The camera scanner confidence is high enough to rename the directory automatically
                 if confidence_pass:
-                    stats['not_in_dir_movable'] += 1
-                    print_ident_result('File not in cam directory, can automatically move to cam dir', 'yellow')
+                    stats['in_unknown_cam_dir_movable'] += 1
+                    # print_ident_result('File in unknown cam directory.. dir can be renamed automatically', 'yellow')
+                    new_dir_name = identified_cam_name
+                    new_full_path = os.path.join(path.parent.parent.resolve(), new_dir_name)
 
-                # Our camera confidence is not high enough to automatically move the file, user must move manually
+                    # rename the parent (directory) to the new name
+                    # TODO: Enable renaming once the directory scanning logic has been integrated directly into the identify function
+                    # path.parent.rename(new_full_path)
+                    print_ident_result(f'File in unknown cam directory.. dir renamed to \'{new_dir_name}\'', 'yellow')
+
+                # the camera scanner confidence is NOT high enough to take any automatic action. manual user intervention required
                 else:
-                    stats['not_in_dir_not_movable'] += 1
-                    print_ident_result('File not in cam directory, and must be moved to cam dir manually', 'red')
+                    stats['in_unknown_cam_dir_not_movable'] += 1
+                    print_ident_result('File in unknown cam directory.. dir cannot be renamed automatically', 'red')
 
-            # TODO: create list with any manual action that must be taken to show at end of process
-            # break
+        # Discovered file not located in an appropriately named camera directory.. action needs to be taken
+        else:
+
+            # We are confident of our camera identification, move the file to an appropriately named folder
+            if confidence_pass:
+                stats['not_in_dir_movable'] += 1
+                print_ident_result('File not in cam directory, can automatically move to cam dir', 'yellow')
+
+            # Our camera confidence is not high enough to automatically move the file, user must move manually
+            else:
+                stats['not_in_dir_not_movable'] += 1
+                print_ident_result('File not in cam directory, and must be moved to cam dir manually', 'red')
+
+        # TODO: create list with any manual action that must be taken to show at end of process
+        # break
+
+
+    shared.scanmedia(
+        Config.directories.raw_footage_root,
+        handle_media_file,
+        Config.known_extensions,
+        Config.directories.include_paths,
+        Config.directories.ignore_paths
+    )
 
 
     print("Summary:")
--- modules/shared.py
+++ modules/shared.py
@@ -1,8 +1,9 @@
 import logging
 import os
+import pathlib
 
 from os import DirEntry
-from typing import Any, Dict, Generator
+from typing import Any, Callable, Dict, Generator
 
 import yaml
 
@@ -42,3 +43,46 @@ def scantree(path, depth=0) -> Generator[tuple[DirEntry[str], int], None, None]:
         else:
             if os.path.exists(entry.path):
                 yield (entry, depth)
+
+
+def scanmedia(
+    root_path: str,
+    process_file: Callable[[DirEntry, int, Callable[[], None]], None],
+    extensions: list[str] = list(),
+    include_paths: list[str] = list(),
+    exclude_paths: list[str] = list(),
+    depth: int = 0
+) -> None:
+    """This is meant to be a reusable function for walking media directory trees"""
+
+    def dir_renamed() -> None:
+        print('hey')
+
+    for entry in os.scandir(root_path):
+        if entry.is_dir(follow_symlinks=False):
+            scanmedia(entry.path, process_file, extensions, include_paths, exclude_paths, depth + 1)
+        else:
+            # if a list of allowed extensions was provided, we skip any files that do not in the approved list
+            if len(extensions) > 0:
+                path = pathlib.Path(entry.path)
+
+                extension = path.suffix[1:]
+                # file_basename = path.name[:-len(extension) - 1]
+
+                if extension not in extensions:
+                    continue
+
+            # skip any paths that are defined in our exclude_paths directive
+            if any(entry.path.startswith(x) for x in exclude_paths):
+                continue
+
+            # if a list of include paths is configued, we require the current path to be in the list
+            # else we skip the current path
+            if len(include_paths) > 0:
+                if not any(entry.path.startswith(x) for x in include_paths):
+                    continue
+
+            print(entry.path)
+                
+            # if we made it this far, we execute the callback to process the file
+            process_file(entry, depth, dir_renamed)
