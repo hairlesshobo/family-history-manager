471f321096d89f4f69bb97d49cc5a35995c0f180
Steve Cross <flip@foxhollow.cc>
2022-12-12 07:04:23 -0500

    Contuing to build c# version

---
+++ src/FoxHollow.HVM.Core/Models/Config.cs
@@ -0,0 +1,10 @@
+namespace FoxHollow.HVM.Core.Classes
+{
+    public class Config
+    {
+        public string VideoRoot { get; set; }
+        public ConfigDirectory Raw { get; set; }
+        public ConfigDirectory WebFootage { get; set; }
+        public ConfigDirectory FinalFootage { get; set; }
+    }
+}
---
+++ src/FoxHollow.HVM.Core/Models/ConfigDirectory.cs
@@ -0,0 +1,10 @@
+namespace FoxHollow.HVM.Core.Classes
+{
+    public class ConfigDirectory
+    {
+        public string Root { get; set; }
+        public string[] Include { get; set; }
+        public string[] Exclude { get; set; }
+        public string[] Extensions { get; set; }
+    }
+}
