137c8fa63c47989b29ce34e92539448f886c641a
Steve Cross <flip@foxhollow.cc>
2022-12-22 07:07:46 -0500

    Removed previous python version now that it is no longer needed as a guide

--- python-scrapped/cli_commands/generate_raw_video_sidecars.py
+++
@@ -1,8 +0,0 @@
-import click
-
-from operations.generate_raw_video_sidecars import GenerateRawVideoSidecars
-
-@click.command("generate-raw-video-sidecars")
-def command() -> None:
-    generator = GenerateRawVideoSidecars()
-    generator.process_source_tree()
